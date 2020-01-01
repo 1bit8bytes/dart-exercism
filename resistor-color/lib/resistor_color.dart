77755811bfb46f8fcf865cd17c3dc426b8bcf9e3
Mateus Siqueira <omateussiqueira@gmail.com>
2020-01-01 10:27:29 -0300

    resistor color

---
+++ resistor-color/lib/resistor_color.dart
@@ -0,0 +1,29 @@
+class ResistorColor {
+  final List<String> colors = [
+    'black',
+    'brown',
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'violet',
+    'grey',
+    'white'
+  ];
+
+  final Map<String, int> _colors = {
+    "black": 0,
+    "brown": 1,
+    "red": 2,
+    "orange": 3,
+    "yellow": 4,
+    "green": 5,
+    "blue": 6,
+    "violet": 7,
+    "grey": 8,
+    "white": 9,
+  };
+
+  int colorCode(String code) => _colors[code];
+}
